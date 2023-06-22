8f31e7abed556a53a658718c1eba0be1bfa32af4
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-06-22 18:02:06 +0300

    create Markup Items

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+
+function createMarkupItems(arr) {
+  return arr
+    .map(
+      ({ original, description, preview }) =>
+        `<li class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </li>`
+    )
+    .join('')
+}
+
+
+galleryList.insertAdjacentHTML('beforeend', createMarkupItems(galleryItems));
 
 console.log(galleryItems);
