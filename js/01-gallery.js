5b4f31a4aa16dc259c534ed2abe0038331a173ea
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-06-25 21:46:20 +0300

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,13 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryList = document.querySelector('.gallery');
 
-
 function createMarkupItems(arr) {
   return arr
     .map(
       ({ original, description, preview }) =>
         `<li class="gallery__item">
-                <a class="gallery__link" href="large-image.jpg">
+                <a class="gallery__link" href="${original}">
                     <img
                     class="gallery__image"
                     src="${preview}"
@@ -18,10 +17,33 @@ function createMarkupItems(arr) {
                 </a>
             </li>`
     )
-    .join('')
+    .join('');
 }
-
 
 galleryList.insertAdjacentHTML('beforeend', createMarkupItems(galleryItems));
 
-console.log(galleryItems);
+galleryList.addEventListener('click', hadlerClickCard);
+
+function hadlerClickCard() {
+  console.log('It was click in the card');
+}
+
+galleryList.onclick = event => {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox
+    .create(
+      `
+        <img width = "1400" height= "900" src ="${event.target.dataset.source}">`
+    )
+    .show();
+
+  window.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+};
