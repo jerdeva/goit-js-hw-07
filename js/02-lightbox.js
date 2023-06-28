44b0132ea2bdce3f3105fcd613197b1c50c18c04
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-06-28 11:49:40 +0300

    02 - lightbox done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    ({ original, description, preview }) =>
+      `      <li class="gallery__item">
+   <a class="gallery__link"  href="${original}" title=""${description}>
+              <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt
+                  title = "${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+let gallery = new SimpleLightbox('.gallery a');
+
+
+galleryList.onclick = event => {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  window.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      gallery.close();
+    }
+  });
+};
